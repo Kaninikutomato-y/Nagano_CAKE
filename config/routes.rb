429a5968abfb30a5d845c4362068ad934b640481
vagrant <vagrant@localhost.localdomain>
2020-09-02 02:41:07 +0000

    login-function

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,6 @@ Rails.application.routes.draw do
   	resources :orders, only: [:new, :create]
   	post 'orders/confirm'
   	get 'orders/thanks'
-
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
