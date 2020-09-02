bbbd87b794d1ae303af9e1447df30f97263c9071
vagrant <vagrant@localhost.localdomain>
2020-09-02 05:14:32 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :clients
-  devise_for :admins
-  
 
-  resources :clients, only: [:show, :edit, :update] do
-  	member do
-  		get "unsubscribe"
-  		patch "unsubscribe"
-  	end
-  end
+
 
   resources :cart_items, only: [:index, :create, :update, :destroy]
   delete 'cart_items' => 'cart_items#destroy_all'
   resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
+
+
+  devise_for :admins, controllers: {
+    sessions: 'admin/admins/sessions',
+    registrations: 'admin/admins/registrations',
+    passwords: 'admin/admins/passwords'
+  }
 
   namespace :admin do
   	resources :products, only: [:new, :create, :edit, :update, :show, :index]
@@ -24,6 +23,11 @@ Rails.application.routes.draw do
   end
 
   scope module: :client do
+    devise_for :clients, controllers: {
+      sessions: 'client/clients/sessions',
+      registrations: 'client/clients/registrations',
+      passwords: 'client/clients/passwords'
+    }
   	resources :products, only: [:index, :show]
   	get 'home/top'
   	root 'home#top'
@@ -33,5 +37,11 @@ Rails.application.routes.draw do
   	get 'orders/thanks'
   end
 
+  resources :clients, only: [:show, :edit, :update] do
+    member do
+      get "unsubscribe"
+      patch "unsubscribe"
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
