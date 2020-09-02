aed78c42f7d69bad98bf1ce98fcc3e4e54f35b78
vagrant <nervous6622@gmail.com>
2020-09-02 08:17:44 +0000

    [add]signup

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,11 +3,18 @@ class ClientsController < ApplicationController
   end
 
   def edit
+    @client = Client.find(params[:id])
   end
 
   def update
   end
 
   def unsubscribe
+  end
+
+  private
+  def client_params
+    params.require(:client).permit(
+      :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :phone_number)
   end
 end
