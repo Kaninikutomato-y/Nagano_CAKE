615f2f64e02d386e13221f1cbbd317c18513c1a2
vagrant <nervous6622@gmail.com>
2020-09-08 10:51:29 +0000

    [fix]products

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,6 +1,6 @@
 class Client::ProductsController < ApplicationController
   def index
-    @products = Product.page(params[:page]).reverse_order
+    @products = Product.joins(:genre).where(genres: {valid_flag: true})
     @genres = Genre.all
   end
 
@@ -9,16 +9,8 @@ class Client::ProductsController < ApplicationController
   	@cart_item = CartItem.new
   end
 
-
-  private
-  def product_params
-    params.require(:product).permit(:name, :explanation, :price, :image, :sale_status, :genre_id)
-  end
-
   def search
     @genre = Genre.find(params[:id])
     @products = @genre.products.order("created_at DESC").reverse_order
   end
 end
-
-
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
 	has_many :products
- 　 validates :name, presence: true
+  validates :name, presence: true
 end
 
