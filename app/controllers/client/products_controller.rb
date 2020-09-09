a68ab9b06a7f25140db28c0e5d1ff6d8e23b234e
vagrant <vagrant@localhost.localdomain>
2020-09-09 03:08:57 +0000

    [start]home

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -10,15 +10,9 @@ class Client::ProductsController < ApplicationController
   end
 
 
-  private
-  def product_params
-    params.require(:product).permit(:name, :explanation, :price, :image, :sale_status, :genre_id)
-  end
-
   def search
     @genre = Genre.find(params[:id])
     @products = @genre.products.order("created_at DESC").reverse_order
   end
+
 end
-
-
