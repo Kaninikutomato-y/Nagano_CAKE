b19e836ad688bd00e48d62f719204dbe3cbb195e
vagrant <nervous6622@gmail.com>
2020-09-09 10:02:19 +0000

    [finish]search

--- app/models/client.rb
+++ app/models/client.rb
@@ -10,15 +10,23 @@ class Client < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :deliveries, dependent: :destroy
 
+
   def self.search(search,word)
   	if search == "forward_match"
-  		@client = Client.where("last_name LIKE?","#{word}%")
+  		@client = Client.where("last_name LIKE?",word + '%')
   	elsif search == "backward_match"
-    	@client = Client.where("last_name LIKE?","%#{word}")
+    	@client = Client.where("first_name LIKE?",'%' + word)
     elsif search == "perfect_match"
-    	@client = Client.where("last_name LIKE?","#{word}")
+      clientlist = []
+      Client.all.each do |client|
+        fullname = client.last_name + client.first_name
+        if fullname == word
+          clientlist.push client.id
+        end
+      end
+      @client = Client.find(clientlist)
     elsif search == "partial_match"
-    	@client = Client.where("last_name LIKE?","%#{word}%")
+    	@client = Client.where("last_name LIKE?",'%' + word + '%').or(Client.where("first_name LIKE?",'%' + word + '%'))
     else
     	@client = Client.all
     end
