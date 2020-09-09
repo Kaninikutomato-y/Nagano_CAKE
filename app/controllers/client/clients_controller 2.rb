86da3e0fe03616d3caa9429668d0098ef51af51c
vagrant <vagrant@localhost.localdomain>
2020-09-09 03:27:29 +0000

    [Add]URL

---
+++ app/controllers/client/clients_controller 2.rb
@@ -0,0 +1,28 @@
+class Client::ClientsController < ApplicationController
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    @client.update(client_params)
+  end
+
+  def unsubscribe
+    @client = Client.find(params[:id])
+    if request.patch?
+      @client.update(is_deleted: true)
+      redirect_to root_path
+    end
+  end
+
+  private
+    def client_params
+      params.require(:client).permit(
+        :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :phone_number)
+    end
+end
