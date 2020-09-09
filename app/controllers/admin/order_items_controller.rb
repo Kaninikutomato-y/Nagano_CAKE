67f7e795df6bcb5501381b16a024fda7a6f8d732
vagrant <vagrant@localhost.localdomain>
2020-09-09 09:41:21 +0000

    [80%]Orders

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,4 +1,12 @@
 class Admin::OrderItemsController < ApplicationController
   def update
+  	@order_items = OrderItem.find(params[:id])
+  	@order_items.update(order_item_params)
+  	redirect_to admin_orders_path
+  end
+
+  private
+  def order_item_params
+  	params.require(:order_item).permit(:production_status)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a8939c25888bff6c925af5d4c7fde81bb84c7b2f68d4f925c49699fdc14760227143f0f8b4d1b38c12c84f08dddfecd71597f532a35ff49232ceba6e9d3c198f'
