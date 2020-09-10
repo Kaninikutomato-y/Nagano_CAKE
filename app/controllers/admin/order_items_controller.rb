fbbc7c5f1ba7b702bf86c8ef9177e9fe5489990a
vagrant <vagrant@localhost.localdomain>
2020-09-10 12:40:31 +0000

    status fix

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,7 +1,21 @@
 class Admin::OrderItemsController < ApplicationController
   def update
-  	@order_items = OrderItem.find(params[:id])
-  	@order_items.update(order_item_params)
+  	order_item = OrderItem.find(params[:id])
+  	if order_item.update(order_item_params)
+  		if order_item.production_status == "制作中"
+  			order_item.order.update(status: 2)
+  		end
+  		order_items = order_item.order.order_items
+  		check = 0
+  		order_items.each do |order_item|
+		  	if order_item.production_status == "制作完了"
+				check += 1
+			end
+		end
+		if check == order_items.length
+			order_item.order.update(status: 3)
+		end
+  	end
   	redirect_to admin_orders_path
   end
 
